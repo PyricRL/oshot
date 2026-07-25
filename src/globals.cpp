c8788476f60b00f594c92f424188369adebb7243
Toni500git <toni500minecraft@gmail.com>
2026-07-25 14:36:13 +0200

    fix macos build (hopefully)

--- src/globals.cpp
+++ src/globals.cpp
@@ -8,18 +8,20 @@
 #include "screenshot_tool.hpp"
 #include "util.hpp"
 
-static StateManager _s;
-
 // Extern variables declariaions
 std::deque<std::string> g_dropped_paths;
 std::unique_ptr<Config> g_config;
 std::unique_ptr<Cache>  g_cache;
-ScreenshotTool          g_ss_tool(std::move(_s));
 bool                    g_is_systray = false;
 int                     g_scr_w{}, g_scr_h{};
 Clipboard               g_clipboard(SessionType::Unknown);
 
 #ifndef DISABLE_PLUGINS
+static StateManager _s;
+ScreenshotTool      g_ss_tool(std::move(_s));
+
 std::unordered_map<std::string, plugin_runtime_t> g_plugins;
 plugin_runtime_t*                                 g_current_plugin;
+#else
+ScreenshotTool g_ss_tool;
 #endif

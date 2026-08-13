847684c178f813618b08ffe2437d32d5bbebaeeb
Toni500git <toni500minecraft@gmail.com>
2026-08-13 19:10:50 +0200

    misc: add/update copyright notice to all source files

--- oshotpm/include/manifest.hpp
+++ oshotpm/include/manifest.hpp
@@ -1,5 +1,5 @@
 /*
- * Copyright 2025 Toni500git
+ * Copyright 2026 Toni500
  *
  * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the
  * following conditions are met:
